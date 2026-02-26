de8cf9552ddc27cf36a13eb00a251f1e208303af
Dimas Maulana Ahmad <97804178+dimsmaul@users.noreply.github.com>
2026-02-27 06:39:15 +0700

    Add Lorem component and integrate it into App

---
+++ src/lorem.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Lorem = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam esse eius deserunt doloremque id iste iusto quisquam vero impedit fuga saepe numquam doloribus a molestiae similique, excepturi accusantium vel corporis.</div>
+  )
+}
+
+export default Lorem
